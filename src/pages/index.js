e0ea8ba97129e15493f1e79674040c841fb4dff4
Jon <jonfisher@lyft.com>
2020-05-06 21:02:29 -0700

    added blog title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+function getTitle(title) {
+  return title;
+}
+
 export default () => {
   return (
     <>
-      Hello world!
+      <h1>{getTitle(UPDATER)}</h1>
       <p><Link to="/blog">View Blog</Link></p>
       <p><Link to="/account">My Account</Link></p>
     </>)
